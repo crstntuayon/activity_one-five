8bcf6e6037225312f90681ab9059681fd084dfd0
crese <crestuayon@gmail.com>
2025-06-06 02:00:19 -0700

    Activity 02: View and Create

--- laravel/app/Http/Controllers/StudentController.php
+++ laravel/app/Http/Controllers/StudentController.php
@@ -3,13 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index()
+    // READ
+    public function myView()
     {
-          $students = Student::all();
-    return view('students.index', compact('students'));
+        $students = Student::all();
+        $users = User::all();
+
+        return view('welcome', compact('students', 'users'));
+    }
+
+    // CREATE
+    public function addNewStudent(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'age' => 'required',
+            'gender' => 'required',
+        ]);
+
+        $add_new = new Student;
+        $add_new->id = $request->id;
+        $add_new->name = $request->name;
+        $add_new->age = $request->age;
+        $add_new->gender = $request->gender;
+        $add_new->save();
+
+
+
+        return back()->with('success', 'Student added successfully');
+    }
+
+    // UPDATE
+    public function updateView($id)
+    {
+        $students = Student::where('id', '=', $id)->get();
+        return view('update', compact('students'));
+    }
+
+
+    public function updateME(Request $request)
+    {
+        Student::where('id', '=', $request->id)->update([
+            'name' => $request->name,
+            'age' => $request->age,
+            'gender' => $request->gender,
+        ]);
+
+        return redirect('/')->with('success', 'Student updated successfully');
+    }
+
+    public function deleteME($id)
+    {
+        Student::where('id', '=', $id)->delete();
+        return back()->with('success', 'Student deleted successfully');
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\StudentController;
+use App\Http\Middleware\AuthCheck;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+// Auth
+
+    // View
+    Route::get('/students', [StudentController::class, 'myView'])->name('std.myView');
+    // Create
+    Route::post('/add-new', [StudentController::class, 'addNewStudent'])->name('std.addNewStudent');
+    // PUT
+    Route::get('/student/update/{id}', [StudentController::class, 'updateView'])->name('std.updateView');
+    Route::post('/update', [StudentController::class, 'updateME'])->name('std.studentUpdate');
+    // DELETE
+    Route::get('/delete/{id}', [StudentController::class, 'deleteME'])->name('std.studentDelete');
+    
+    
+    
+   
+    // Logout
   
-    Route::get('/students', [StudentController::class, 'index']);
+
